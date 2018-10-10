956879be1a3277a452cd6da84e3f3664207e8c6c
Nils Lefever <nilslef@gmail.com>
2018-10-10 18:18:48 +0200

    First commit

---
+++ src/Rental/rental/ReservationException.java
@@ -0,0 +1,13 @@
+package Rental.rental;
+
+public class ReservationException extends Exception {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ReservationException(String string) {
+        super(string);
+    }
+}
